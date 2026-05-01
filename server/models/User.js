736c6f923bd91218619dd385b8a992b75184b30d
pranav1031 <pranavsharma1489@gmail.com>
2026-05-01 13:29:28 +0530

    user model setup

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true, lowercase: true},
+    password: {type: String, required: true, minlength: 6},
+    role: {type: String, enum: ["user", "organizer"], default: "user"}
+  },
+  {timestamps: true}
+)
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
